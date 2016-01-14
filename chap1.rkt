1d1e208f11a32c2c9be8c46b964ecbef5221fa65
tonychen <qingpengchen2011@gmail.com>
2016-01-14 15:06:22 +0800

    define number-elements

--- chap1.rkt
+++ chap1.rkt
@@ -105,5 +105,15 @@
                    sexp)
                (subst3 old new sexp)))
          slist)))
-                       
-          
+
+;;section 1.3
+;;number-elements-from: ListOf(SchemeVal) * Int -> ListOf(List(Int,SchemeVal))
+(define number-elements-from
+  (lambda (lst n)
+    (if (null? lst)
+        '()
+        (cons (list n (car lst))
+              (number-elements-from (cdr lst) (+ n 1))))))
+(define number-elements
+  (lambda (lst)
+    (number-elements-from lst 0)))
