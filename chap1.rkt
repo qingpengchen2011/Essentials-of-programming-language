eeee1e75593476fbdaf8ed063f355c5aeb98ba29
tonychen <qingpengchen2011@gmail.com>
2016-01-14 16:48:40 +0800

    exercise1.21

--- chap1.rkt
+++ chap1.rkt
@@ -189,3 +189,12 @@
             0)
         (count-occurrences s sexp))))
 
+;;exercise1.21
+(define product
+  (lambda (sos1 sos2)
+    (if (null? sos1)
+        '()
+        (append (map (lambda (sym2)
+                       (list (car sos1) sym2))
+                     sos2)
+                (product (cdr sos1) sos2)))))
