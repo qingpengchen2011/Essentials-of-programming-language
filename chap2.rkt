c1c0c17eb400641047a35fa4cd44474e4e378f70
tonychen <qingpengchen2011@gmail.com>
2016-01-18 09:41:46 +0800

    env data structure representation

---
+++ chap2.rkt
@@ -0,0 +1,38 @@
+#lang eopl
+
+
+
+
+;;;section2.2.2
+(define empty-env
+  (lambda ()
+    (list 'empty-env)))
+
+(define extend-env
+  (lambda (var val env)
+    (list 'extend-env var val env)))
+
+(define apply-env
+  (lambda (env search-var)
+    (cond ((eqv? 'empty-env (car env))
+           (report-no-binding-found search-var))
+          ((eqv? 'extend-env (car env))
+           (let ((saved-var (cadr env))
+                 (saved-val (caddr env))
+                 (saved-env (cadddr env)))
+             (if (eqv? search-var saved-var)
+                 saved-val
+                 (apply-env saved-env search-var))))
+          (else
+           report-invalid-env env))))
+
+
+(define report-no-binding-found
+  (lambda (search-var)
+    (eopl:error 'apply-env "No binding for ~s" search-var)))
+
+(define report-invalid-env
+  (lambda (env)
+    (eopl:error 'apply-env "Bad environments ~s" env)))
+
+
