84e8a56542e7963cdf59f0cb76665c9213d408ba
tonychen <qingpengchen2011@gmail.com>
2016-01-19 10:48:14 +0800

    exercise2.21

---
+++ chap2-from-section2.4-to-end.rkt
@@ -0,0 +1,26 @@
+#lang eopl
+;;section 2.4
+
+(define identifier? symbol?)
+
+(define-datatype lc-exp lc-exp?
+  (var-exp (var identifier?))
+  (lambda-exp (bound-var identifier?)
+              (body lc-exp?))
+  (app-exp (rator lc-exp?)
+           (rand lc-exp?)))
+
+;;exercise2.21
+(define-datatype environment environment?
+  (empty-env)
+  (extend-env (var identifier?)
+              (val (lambda (v) #t))
+              (saved-env environment?)))
+(define has-binding?
+  (lambda (search-var env)
+    (cases environment env
+      (empty-env () #f)
+      (extend-env (var val saved-env)
+                  (if (eqv? var search-var)
+                      #t
+                  (has-binding? search-var saved-env))))))
