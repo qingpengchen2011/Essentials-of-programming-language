c693000ee40cf31adafced65e880d591937a985f
tonychen <qingpengchen2011@gmail.com>
2016-01-19 12:20:49 +0800

    exercise2.25

--- chap2-from-section2.4-to-end.rkt
+++ chap2-from-section2.4-to-end.rkt
@@ -47,3 +47,43 @@
       (interior-node (key left right)
                      (list 'interior-node key (bintree-to-list left) (bintree-to-list right))))))
 
+;;exercise2.25
+(define tree-1
+  (interior-node 'foo (leaf-node 2) (leaf-node 3)))
+(define tree-2
+  (interior-node 'bar (leaf-node -1) tree-1))
+(define tree-3
+  (interior-node 'baz tree-2 (leaf-node 1)))
+
+(define max-interior
+  (lambda (btree)
+    (define search-max-interior
+      (lambda (bt)
+        (cases bintree bt
+          (leaf-node (num) (list num 'leaf-node num 'leaf-node)) ;;(current-sum current-node max-sum max-node)
+          (interior-node (key left right)
+                         (letrec ((left-data (search-max-interior left))
+                               (right-data (search-max-interior right))
+                               (left-current-sum (car left-data))
+                               (left-current-sum-node (cadr left-data))
+                               (left-max-sum (caddr left-data))
+                               (left-max-node (cadddr left-data))
+                               (right-current-sum (car right-data))
+                               (right-current-sum-node (cadr right-data))
+                               (right-max-sum (caddr right-data))
+                               (right-max-node (cadddr right-data)))
+                           (letrec ((current-sum (+ left-current-sum right-current-sum))
+                                    (child-max (if (>= left-max-sum right-max-sum)
+                                                   (list left-max-sum left-max-node)
+                                                   (list right-max-sum right-max-node)))
+                                    (current-max (if (>= current-sum (car child-max))
+                                                     (list current-sum key)
+                                                     child-max)))
+                             (cons current-sum
+                                   (cons key
+                                         current-max))))))))
+     (cadddr (search-max-interior btree))))
+                                 
+                    
+                               
+                                 
